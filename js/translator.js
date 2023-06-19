481213d02e7c002fe1dce6a66caf9e5f5c1db6a1
Renato Palominos <82245870+ReenaPalominos@users.noreply.github.com>
2023-06-18 21:34:22 -0400

    UPLOAD: v1.0
    Traductor base

---
+++ js/translator.js
@@ -0,0 +1,8 @@
+function accessEndpoint() {
+    // Obtener la URL del endpoint deseado
+    var endpointURL = "https://www.ejemplo.com/endpoint";
+  
+    // Realizar alguna acción con la URL, por ejemplo, redireccionar a la URL
+    window.location.href = endpointURL;
+  }
+  
