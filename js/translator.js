41d5d8f0256e5699b76dccbb407cf83774f5e7bb
Renato Palominos <rpalominos@utem.cl>
2023-06-21 22:11:00 -0400

    UPDATE: v1.4 - Buttons

--- js/translator.js
+++ js/translator.js
@@ -1,8 +1,62 @@
-function accessEndpoint() {
-    // Obtener la URL del endpoint deseado
-    var endpointURL = "https://www.ejemplo.com/endpoint";
-  
-    // Realizar alguna acción con la URL, por ejemplo, redireccionar a la URL
-    window.location.href = endpointURL;
+// Función para acceder al micrófono y convertir la voz a texto
+function accessMicrophone() {
+  // Comprobar si el navegador admite el reconocimiento de voz
+  if (
+    !("SpeechRecognition" in window) &&
+    !("webkitSpeechRecognition" in window)
+  ) {
+    alert("El reconocimiento de voz no es compatible con este navegador.");
+    return;
   }
-  
+
+  const recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
+
+  // Configuración de reconocimiento de voz
+  recognition.lang = "es-ES"; // Establece el idioma de entrada, en este caso, español
+  recognition.continuous = false; // Reconocimiento de voz único en lugar de continuo
+
+  // Evento que se dispara cuando se obtiene un resultado del reconocimiento de voz
+  recognition.onresult = function (event) {
+    const transcript = event.results[0][0].transcript;
+    document.getElementById("input-text").value = transcript; // Agrega el texto reconocido al cuadro de texto de entrada
+  };
+
+  // Evento que se dispara si hay un error en el reconocimiento de voz
+  recognition.onerror = function (event) {
+    console.error("Error en el reconocimiento de voz: ", event.error);
+  };
+
+  // Iniciar el reconocimiento de voz
+  recognition.start();
+}
+
+// Función para que el computador lea el texto escrito en el segundo textarea
+function speakText() {
+  const text = document.getElementById("output-text").value;
+  const utterance = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(utterance);
+}
+
+function translateText() {
+  const inputText = document.getElementById("input-text").value;
+
+  // Realizar la solicitud al backend
+  fetch("localhost:8000", {
+    method: "POST", // Puedes utilizar el método HTTP adecuado (POST, GET, etc.)
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: inputText }), // Enviar el texto a traducir en formato JSON
+  })
+    .then((response) => response.json()) // Convertir la respuesta a JSON
+    .then((data) => {
+      const translatedText = data.translatedText; // Obtener el texto traducido de la respuesta
+
+      // Mostrar el texto traducido en el segundo textarea
+      document.getElementById("output-text").value = translatedText;
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud al backend: ", error);
+    });
+}
